49909bea5dfe25658c7b1da67fae094f0944702b
Omenkish <omenkish@gmail.com>
2020-06-28 20:46:45 +0100

    Implement functionality to check employee's absence status and type

--- js_edition/src/absences.js
+++ js_edition/src/absences.js
@@ -1,3 +1,4 @@
+import moment from 'moment';
 import { absences, members } from './api';
 
 export const allAbsencesWithNames = async () => {
@@ -17,14 +18,49 @@ export const allAbsencesWithNames = async () => {
   }
 }
 
-const getMemberById = async (id) => {
+export const employeeAbsenceStatus = async (employee, absences = allAbsencesWithNames) => {
+  const status = {
+    vacation: (member) => `${member} is on vacation`,
+    sickness: (member) => `${member} is sick`,
+    default: (member) => `${member} is currently NOT absent`,
+  };
   
   try {
+    const allAbsences = await absences();
+    const absence = employeeAbsence(employee.userId, allAbsences);
+  
+    // Return absence type if employee has any absence or null otherwise
+    const type = absence && absence.type || null;
+  
+    return status[type || 'default'](employee.name)
+  } catch (error) {
+    throw new Error(error);
+  }
+}
+
+const getMemberById = async (id) => {
+  try {
     let allMembers = await members();
-    let employee = allMembers.find((member) => member.userId === id) 
+    const employee = allMembers.find((member) => member.userId === id)
     return employee
   } catch (error) {
-    console.log('ERROR:::', error);
+    throw new Error(error);
   }
 
+}
+
+const employeeAbsences = (employeeId, absences) => {
+  
+  return absences.filter((absence) => absence.userId === employeeId);
+}
+
+const employeeAbsence = (employeeId, absences) => {
+  try {
+    const allEmployeeAbsences = employeeAbsences(employeeId, absences);
+
+    return allEmployeeAbsences.find((absence) => moment(absence.endDate) >= new Date());
+  } catch (error) {
+    throw new Error(error);
+  }
+  
 }
--- js_edition/src/absences.spec.js
+++ js_edition/src/absences.spec.js
@@ -1,8 +1,9 @@
 import assert from 'assert';
-import { allAbsencesWithNames } from './absences';
+import { allAbsencesWithNames, employeeAbsenceStatus } from './absences';
+import { members } from './api';
 import { everyItemContainsKey} from './api.spec'
 
-describe('all_absences_with_names', async () => {
+describe('allAbsencesWithNames', async () => {
   describe('every absence has key', () => {
     [
       'admitterNote',
@@ -22,3 +23,54 @@ describe('all_absences_with_names', async () => {
     });
   });
 });
+
+describe('employeeAbsenceStatus', () => {
+  it('prints the right message if employee is not absent', async () => {
+    const allMembers = await members();
+    const member = allMembers[0];
+    const response = await employeeAbsenceStatus(member);
+
+    assert(response, `${member.name} is NOT absent`);
+  })
+
+  const absenceData = async (member, type) => {
+    const data = {
+      admitterId: null,
+      admitterNote: "",
+      confirmedAt: "2016-12-12T18:03:55.000+01:00",
+      createdAt: "2016-12-12T14:17:01.000+01:00",
+      crewId: 352,
+      endDate: "2021-01-13",
+      id: 2351,
+      memberNote: "",
+      rejectedAt: null,
+      startDate: "2017-01-13",
+      type: type,
+      userId: member.userId,
+      employeeName: member.name
+    };
+    return [data];
+  }
+
+  describe('employee is on vacation', () => {
+    it('prints the right message if employee is on vacation', async () => {
+      const allMembers = await members();
+      const member = allMembers[0];
+
+      const response = await employeeAbsenceStatus(member, absenceData.bind(null, member, 'vacation'));
+      assert.strictEqual(response, `${member.name} is on vacation`);
+      
+    })
+  });
+
+  describe('employee is sick', () => {
+    it('prints the right message if employee is sick', async () => {
+      const allMembers = await members();
+      const member = allMembers[0];
+
+      const response = await employeeAbsenceStatus(member, absenceData.bind(null, member, 'sickness'));
+      assert.strictEqual(response, `${member.name} is sick`);
+      
+    })
+  });
+})
